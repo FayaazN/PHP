a38cfbfdf52ca5da60e8b3b3a5356a1b737f3fea
fayaazn <fnabie@roc-dev.com>
2018-05-22 19:52:16 +0200

    PHP Fayaaz Nabie

---
+++ 4. Controlestructuren en loops/4.1/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fanab
+ * Date: 22-5-2018
+ * Time: 19:46
+ */
+
+$a = 6;
+$b = 7;
+
+if ($a > $b) {
+    //voer dit uit;
+} else {
+    //voer dit uit;
+}
--- h1/010/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: fanab
- * Date: 22-5-2018
- * Time: 13:55
- */
-echo "hello world";
-
